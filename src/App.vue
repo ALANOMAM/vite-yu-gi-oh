2652dfdb6275d53b7eabaf7c93984336a3f9e30e
alan_omam <omamalan@hotmail.it>
2024-03-04 14:45:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+ import AppContent from './components/AppContent.vue'
+
+export default{
+
+    data(){
+      return{
+
+      }
+    }
+ 
+
+}
+</script>
+
+<template>
+    <h1>prova</h1>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,17 @@
+
+<script>
+
+export default{
+    name:'AppContent'
+ 
+
+}
+</script>
+
+<template>
+    <h1>prova</h1>
+</template>
+
+<style>
+
+</style>
